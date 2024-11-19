7ac1c2751b907b13582d6f5c8e8cbbffb6896b91
SergioBejarano <sergio2018.bejarano>
2024-11-18 19:03:26 -0500

    Actualizar datos y obtener usuarios por paginación

--- src/main/java/edu/eci/cvds/UserManagement/repositories/StudentRepository.java
+++ src/main/java/edu/eci/cvds/UserManagement/repositories/StudentRepository.java
@@ -2,7 +2,12 @@ package edu.eci.cvds.UserManagement.repositories;
 import edu.eci.cvds.UserManagement.model.Responsible;
 import edu.eci.cvds.UserManagement.model.Student;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -18,4 +23,22 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
 
+
+    /**
+     * Retrieves a paginated list of students.
+     *
+     * @param pageable The Pageable object containing pagination information.
+     * @return A Page of students.
+     */
+    Page<Student> findAll(Pageable pageable);
+
+    /**
+     * Updates the course of a student by their ID.
+     *
+     * @param id         The ID of the student.
+     * @param courseName The new course name.
+     */
+    @Modifying
+    @Query("UPDATE Student s SET s.courseName = :courseName WHERE s.id = :id")
+    void updateStudentCourse(@Param("id") String id, @Param("courseName") String courseName);
 }
